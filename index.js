7cc7e11cf756041660a24f57e73cac82aa8ccfab
cyio <icaner@qq.com>
2018-07-19 15:53:19 +0800

    update

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const path = require('path')
 
 const weiboHelper = weiboer.init(path.resolve(__dirname, './config.json'))
 // weiboHelper.publish('微博内容', ['./images/01.jps', './images/02.jps'])
-weiboHelper.publish('微博内容')
+weiboHelper.publish('微博内容' + new Date())
